823d8dfc4f335dfa6b065c321f106b3d9d5d9ee6
yindaheng98 <yindaheng98@gmail.com>
2022-06-03 23:05:19 +0800

    My first page

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
+	"github.com/yindaheng98/dion-frout/pkg/controller"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
-
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
-	w.ShowAndRun()
+	fmt.Println(controller.GetNatsAddr(a))
 }
--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,4 +1,48 @@
 package controller
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/cloudwebrtc/nats-discovery/pkg/discovery"
+	pb "github.com/yindaheng98/dion/proto"
+	"log"
+)
+
+type Client interface {
+	Connect(nats_addr string) error
+	GetNodes() map[string]discovery.Node
+	SwitchNode(id string)
+	SwitchSession(session *pb.ClientNeededSession)
+}
+
 type Controller struct {
+	cli Client
+}
+
+func NewController(cli Client) Controller {
+	return Controller{
+		cli: cli,
+	}
+}
+
+func Control(a fyne.App, cli Client) {
+}
+
+func GetNatsAddr(a fyne.App) string {
+	addr := "nats://127.0.0.1:4222"
+	w := a.NewWindow("dion System - Please give a NATS Address")
+	label := widget.NewLabel("NATS Address:")
+	input := widget.NewEntry()
+	input.SetPlaceHolder(addr)
+	connect := widget.NewButton("Connect!", func() {
+		if input.Text != "" {
+			addr = input.Text
+		}
+		log.Println("Connecting: ", addr)
+		w.Close()
+	})
+	w.SetContent(container.NewVBox(label, input, connect))
+	w.ShowAndRun()
+	return addr
 }
