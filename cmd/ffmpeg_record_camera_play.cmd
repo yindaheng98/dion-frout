0f1f290da2a8923e8728e72ad3d689a1483f47f9
yindaheng98 <yindaheng98@gmail.com>
2022-06-14 18:52:06 +0800

    ffmoeg and ffplay test

---
+++ cmd/ffmpeg_record_camera_play.cmd
@@ -0,0 +1,2 @@
+.\ffmpeg.exe -f dshow -i video="@device_pnp_\\?\usb#vid_2bdf&pid_028a&mi_00#6&1d424522&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global" -s 1280x720 -vcodec libvpx -b:v 3M -f ivf output.ivf
+.\ffplay.exe -f ivf -i output.ivf
