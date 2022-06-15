2ff8d35cfeff346cb89ddd2116e7f14da0055329
yindaheng98 <yindaheng98@gmail.com>
2022-06-15 21:11:43 +0800

    main ok

---
+++ cmd/aliyun/isglb/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/yindaheng98/dion-frout/cmd/aliyun"
+	"github.com/yindaheng98/dion/algorithms"
+	"github.com/yindaheng98/dion/pkg/isglb"
+
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/pion/ion-log"
+)
+
+var (
+	conf = isglb.Config{}
+	file string
+)
+
+func showHelp() {
+	fmt.Printf("Usage:%s {params}\n", os.Args[0])
+	fmt.Println("      -c {config file}")
+	fmt.Println("      -h (show help info)")
+	os.Exit(-1)
+}
+
+func main() {
+	flag.StringVar(&file, "c", "cmd/islb/islb.toml", "config file")
+	help := flag.Bool("h", false, "help info")
+	flag.Parse()
+	if *help {
+		showHelp()
+	}
+
+	err := conf.Load(file)
+	if err != nil {
+		fmt.Printf("config file %s read failed. %v\n", file, err)
+		showHelp()
+	}
+
+	fmt.Printf("config %s load ok!\n", file)
+
+	log.Init(conf.Log.Level)
+
+	log.Infof("--- starting isglb node ---")
+	node := isglb.New(func() algorithms.Algorithm {
+		return aliyun.StupidAlgorithm{}
+	})
+	if err := node.Start(conf); err != nil {
+		log.Errorf("isglb start error: %v", err)
+		os.Exit(-1)
+	}
+	defer node.Close()
+
+	// Press Ctrl+C to exit the process
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	<-ch
+}
